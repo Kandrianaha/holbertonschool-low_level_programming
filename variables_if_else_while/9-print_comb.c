958cb320dcfae9cf70d4f49894946031ca30102b
Kandrianaha <k.andria@icloud.com>
2025-06-20 07:38:04 +0000

    Print combination

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,13 +10,13 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-
 		if (num < 9)
 		{
 			putchar(',');
+			putchar(' ');
 		}
 	}
-	
+
 	putchar('\n');
 	return (0);
 }
