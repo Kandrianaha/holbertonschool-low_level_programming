c36895964a2f79ad8151819eb2cb3812dac7f8a7
Kandrianaha <k.andria@icloud.com>
2025-06-21 09:02:43 +0000

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point 
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,4 +15,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-}	
+}
