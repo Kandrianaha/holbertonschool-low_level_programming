05cac4733646552334db91ae70a10d790519a359
Kandrianaha <k.andria@icloud.com>
2025-06-21 10:00:38 +0000

    lowercase alphabet

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+/**
+ * _islower - all lowercase characters
+ * @c: The character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
