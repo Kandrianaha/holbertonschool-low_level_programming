edd09e58ae257da86b07193481ed9cce254a81b8
Kandrianaha <k.andria@icloud.com>
2025-06-24 07:37:11 +0000

    function that checks for digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * int_isdigit(int c) - function that checks for digit
+ * @c: character
+ * Return: 1 if c is a digit
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
