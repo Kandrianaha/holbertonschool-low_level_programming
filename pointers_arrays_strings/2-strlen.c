1b7ac7b07bf42714093349bb14c641b9829b3805
Kandrianaha <k.andria@icloud.com>
2025-06-26 10:54:55 +0000

    fixing whitespace

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,30 @@
 #include "main.h"
 /**
- * _strlen - function that returns the length of a string
+ *_strlen - returns the length of a string
  * @s: string
- * Return: length of a string;
+ *Return: returns lenght;
  */
 int _strlen(char *s)
 {
-	int count, inc;
-	inc = 0;
-	for (count = 0; s[count] != '\0'; count++)
-		inc++;
+int count, inc;
+inc = 0;
+for (count = 0; s[count] != '\0'; count++)
+inc++;
 
-	return (inc);
+return (inc);
 }
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
