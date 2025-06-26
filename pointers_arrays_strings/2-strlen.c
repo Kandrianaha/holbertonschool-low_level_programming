c5bb1384d1ecbef3bed31b7474d33dda1dcab73a
Kandrianaha <k.andria@icloud.com>
2025-06-26 10:35:35 +0000

    trying to push

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value of the int it points to
+ * @n: pointer to an int
+ * Retunr: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,2 +1,4 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _strlen(char *s);
