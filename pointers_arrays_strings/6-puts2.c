a17ab051dda96bb51e59074e8757d7f04eaca514
Kandrianaha <k.andria@icloud.com>
2025-06-27 06:24:19 +0000

    fixing line 3

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string, starting with the first character followed by a new line
+ * puts2 - prints every other character of a string
  * @str: the sttring to be printed
  */
 void puts2(char *str)
