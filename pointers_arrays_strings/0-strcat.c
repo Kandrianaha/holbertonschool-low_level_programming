e9b3c1fa2db1ca39f8259562bb2de9e5a4d2c978
Kandrianaha <k.andria@icloud.com>
2025-06-27 11:06:46 +0000

    betty doc fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * _strcat - concatenates two strings
  * @dest: destination string
  * @src: source string
- * return: pointer to dest
+ * Return: pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
