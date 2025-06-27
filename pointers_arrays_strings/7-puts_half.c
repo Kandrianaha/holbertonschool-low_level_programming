da90020a4e6ee16519f71f54d7d63edf34925099
Kandrianaha <k.andria@icloud.com>
2025-06-27 06:53:17 +0000

    fixing script

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,18 @@ void puts_half(char *str)
 	int len = 0;
 	int start;
 
-	while (str[len] != '\0')
+	if (str == 0)
+		return;
 
-		if (len % 2 == 0)
-			start = len / 2;
-		else
-			start= (len - 1) / 2 + 1;
+	if (len % 2 == 0)
+{
+	start = len / 2;
+}
+		
+	else
+{
+	start= (len - 1) / 2 + 1;
+}
 
 	for (i = start; i < len; i++)
 		_putchar(str[i]);
