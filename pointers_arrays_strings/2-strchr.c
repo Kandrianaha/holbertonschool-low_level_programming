7047ff8862dbf9474c3900cb5829f5c172c402a2
Kandrianaha <k.andria@icloud.com>
2025-07-07 09:29:40 +0000

    fixing whitespace

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 /**
  * _strchr - locates a character in a string
- * @s: the string to search 
+ * @s: the string to search
  * @c: the character to find
  * Return: pointer to the first occurence of c in s, or NULL if not found
  */
