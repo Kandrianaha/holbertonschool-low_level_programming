eb80cb8cc495330e9a460d9fa36ec7f86773278d
Kandrianaha <k.andria@icloud.com>
2025-07-11 08:54:05 +0000

    space around +

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n > len2)
 		n = len2;
 
-	new_str = malloc(len1 + n +1);
+	new_str = malloc(len1 + n + 1);
 	if (new_str == NULL)
 		return (NULL);
 
