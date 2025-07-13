e0b77f133ff6b306c20c7f4885c8984fd5ee207f
Kandrianaha <k.andria@icloud.com>
2025-07-13 07:52:24 +0000

    fixing

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <string.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,17 +13,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	char *new_name, *new_owner;
+	int i;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	new_name = malloc(strlen(owner) +1);
+	new_name = malloc(strlen(name) + 1);
 	if (new_name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	for (i = 0; name[i]; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
 
 	new_owner = malloc(strlen(owner) + 1);
 	if (new_owner == NULL)
@@ -32,13 +36,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(d);
 		return (NULL);
 	}
-
-	strcpy(new_name, name);
-	strcpy(new_owner, owner);
+	for (i = 0; owner[i]; i++)
+		new_owner[i] = owner[i];
+	new_owner[i]= '\0';
 
 	d->name = new_name;
 	d->age = age;
-	d->owner= new_owner;
+	d->owner = new_owner;
 
 	return (d);
 }
