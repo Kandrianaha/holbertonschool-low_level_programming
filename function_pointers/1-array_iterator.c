4e75f6b4a15858850656ec5d90dfe45371cb054d
Kandrianaha <k.andria@icloud.com>
2025-07-16 06:37:48 +0000

    betty style

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - executes function given as parameter on element of array
  * @size: size of the array
  * @array: pointer to the array
  * @action: pointer to the function
