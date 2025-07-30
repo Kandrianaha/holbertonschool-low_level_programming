ee8bb96fa8c0e57408891658900b8211c1e3066e
Kandrianaha <k.andria@icloud.com>
2025-07-30 10:13:05 +0000

    add node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,10 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
 	list_t *current;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -27,7 +31,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->len = strlen(new_node->str);
+	new_node->len = len;
 	new_node->next = NULL;
 
 	if (*head == NULL)
