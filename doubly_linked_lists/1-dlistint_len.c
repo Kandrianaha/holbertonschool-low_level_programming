2a09da936d62524ad2c1abd151454dda232cdbdd
Kandrianaha <k.andria@icloud.com>
2025-07-31 09:49:50 +0000

    dlistint_len

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * dlistint_len - returns the number of elements in q dlistint_t list
+ * @h: pointer to the head of the dlistint_t list
+ * Retunr: Number of nodes in the list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
