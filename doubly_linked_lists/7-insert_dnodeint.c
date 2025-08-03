63832f6f890ae0253e49d37e9878d983d16fd449
Kandrianaha <k.andria@icloud.com>
2025-08-03 01:39:06 +0000

    betty style

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -22,7 +22,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL; 
+	new_node->next = NULL;
 	new_node->prev = NULL;
 
 	if (idx == 0)
@@ -49,6 +49,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current->next)
 		current->next->prev = new_node;
 	current->next = new_node;
-
 	return (new_node);
 }
