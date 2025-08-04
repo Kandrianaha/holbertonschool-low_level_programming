7f36e1218cb098419901cb01a6e599e8a976147c
Kandrianaha <k.andria@icloud.com>
2025-08-04 07:24:03 +0000

    copy file

---
+++ file_io/3-cp.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - print error message and exit with code
+ * @code: exit code
+ * @format: error message format
+ * @arg: argument to print in message
+ */
+void error_exit(int code, const char *format, const char *arg)
+{
+	dprintf(STDERR_FILENO, format, arg);
+	exit(code);
+}
+
+/**
+ * main - copies content from one file to another
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success, exit on failure
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, r_len, w_len;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp filee_from file_to\n", "");
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		error_exit(99, "Error: Can't write to %s\n", argv[2]);
+	}
+
+	while ((r_len = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w_len = write(fd_to, buffer, r_len);
+		if (w_len != r_len)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to %s\n", argv[2]);
+		}
+	}
+	if (r_len == -1)
+	{
+		close(fd_from);
+		close(fd_to);
+			error_exit(98, "Error: Can't read from file %s\n", argv[1]);
+	}
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", argv[1]);
+
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd %d\n", argv[2]);
+
+	return (0);
+}
