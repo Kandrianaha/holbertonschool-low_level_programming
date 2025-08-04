94bb479a4179da0a8b9a070f60194f54d2c0712d
Kandrianaha <k.andria@icloud.com>
2025-08-04 06:32:00 +0000

    betty style

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -34,7 +34,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		free(buffer);
 		close(fd);
-		return(0);
+		return (0);
 	}
 
 	write_bytes = write(STDOUT_FILENO, buffer, read_bytes);
