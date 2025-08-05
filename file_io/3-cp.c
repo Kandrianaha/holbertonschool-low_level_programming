06127ecde36bc8cc17a328d87a7ae0d7619bcb58
Kandrianaha <k.andria@icloud.com>
2025-08-05 08:18:55 +0000

    fixing code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,7 +17,7 @@ void error_exit(int code, const char *format, ...)
 {
 	va_list args;
 	va_start(args, format);
-	vdprintf(STDERR_FILENO, format, args);
+	vfprintf(stderr, format, args);
 	va_end(args);
 	exit(code);
 }
